79766281cdd6bcbf37ed6ddb260a16b9644bb915
kesalin <kesalin@gmail.com>
2014-10-20 17:36:44 +0800

    Add open in blank.

---
+++ source/javascripts/open-in-blank.js
@@ -0,0 +1,9 @@
+function addBlankTargetForLinks () {
+  $('a[href^="http"]').each(function(){
+		$(this).attr('target', '_blank');
+	});
+}
+
+$(document).bind('DOMNodeInserted', function(event) {
+	addBlankTargetForLinks();
+});

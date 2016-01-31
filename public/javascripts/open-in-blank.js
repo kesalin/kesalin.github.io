d9370fcf67ad53d131333735d2a90d9629eb67c7
kesaln <kesalin@gmail.com>
2016-01-31 13:58:25 +0800

    Site updated at 2016-01-31 05:58:25 UTC

--- public/javascripts/open-in-blank.js
+++
@@ -1,9 +0,0 @@
-function addBlankTargetForLinks () {
-  $('a[href^="http"]').each(function(){
-		$(this).attr('target', '_blank');
-	});
-}
-
-$(document).bind('DOMNodeInserted', function(event) {
-	addBlankTargetForLinks();
-});
